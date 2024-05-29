a830109d50895ba2ef47da02b325b031db316ebc
MarcioArak <marcio.arakaki22@gmail.com>
2024-05-28 21:40:13 -0700

    update connection

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: `https://jkljlkj.app/api/`,
+  baseURL: `https://social-media-production-5b7b.up.railway.app/api/`,
   withCredentials: true,
 });
